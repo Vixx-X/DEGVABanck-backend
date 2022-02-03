bc2157206e6b2e9675c1943c564a36575d56b314
danielviei <danielviei@hotmail.com>
2022-02-03 01:31:08 -0400

    feat: added models

--- degvabank/degvabank/apps/credit_card/models.py
+++ degvabank/degvabank/apps/credit_card/models.py
@@ -2,3 +2,12 @@
 Hereda de card
 user (referencia)
 """
+from django.db import models
+from apps.card.models import Card
+from apps.user.models import User
+
+class CreditCard(Card):
+    user = models.ForeignKey(
+        User,
+        to_field='document_id',
+    )
--- degvabank/degvabank/apps/transaction/models.py
+++ degvabank/degvabank/apps/transaction/models.py
@@ -6,3 +6,38 @@ date
 target_account (reference)
 source_account (reference)
 """
+import uuid
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from degvabank.degvabank.apps.account.models import Account
+
+class Transaction(models.Model):
+    id = models.UUIDField(
+        _("transaction id"),
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+
+    amount = models.DecimalField(
+        decimal_places=2,
+    )
+
+    reason = models.CharField(
+        _("reason why the transaction is being carried out"),
+        max_length=50,
+    )
+
+    date = models.DateField()
+
+    target_account = models.ForeignKey(
+        Account,
+        to_field='account_num'
+    )
+
+    source_account = models.ForeignKey(
+        Account,
+        to_field='account_num'
+    )
