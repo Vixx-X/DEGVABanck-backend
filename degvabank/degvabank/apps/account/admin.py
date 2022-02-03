32bd6435a1d1d9cb9b2ba5ebe1d4160dda257fc1
vixx_ <vittorio.adesso@gmail.com>
2022-02-03 07:53:59 -0400

    feat: account better ui

--- degvabank/degvabank/apps/account/admin.py
+++ degvabank/degvabank/apps/account/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Account
 
 # Register your models here.
-admin.site.register(Account)
+@admin.register(Account)
+class AccountListAdmin(admin.ModelAdmin):
+    search_fields = ("user__username__startswith", "user__first_name__startswith", "id__startswith")
+    list_display = ("id", "status", "type", "user", "creation_date")
+    list_filter = ("status", "type", "creation_date")
