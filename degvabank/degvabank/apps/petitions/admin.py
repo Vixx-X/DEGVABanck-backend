3dfe71b586bd25b05ddd546cd1e9682cd4d01fb0
vixx_ <vittorio.adesso@gmail.com>
2022-03-01 23:15:22 -0400

    feat: petition admin

--- degvabank/degvabank/apps/petitions/admin.py
+++ degvabank/degvabank/apps/petitions/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Petition
 
-# Register your models here.
-admin.site.register(Petition)
+@admin.register(Petition)
+class PetitionListAdmin(admin.ModelAdmin):
+    list_display = ("id", "reason","status", "date_processed", "date_created", "user")
+    list_filter = ("reason", "date_processed", "date_created", "status", "user")
