d2bce6d408729b09ac6d35fe245d6815ecc5895d
vixx_ <vittorio.adesso@gmail.com>
2022-03-16 21:28:25 -0400

    feat: enabling session auth

--- degvabank/degvabank/settings/shared.py
+++ degvabank/degvabank/settings/shared.py
@@ -235,6 +235,7 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "DEFAULT_FILTER_BACKENDS": [
